bdf0905e6113d17c070bc5b6923a32c18a0302e8
Julien Nioche <julien@digitalpebble.com>
2010-01-18 12:04:43 +0000

    added utility to get the weights from a liblinear model

--- src/com/digitalpebble/classification/util/ModelUtils.java
+++ src/com/digitalpebble/classification/util/ModelUtils.java
@@ -2,17 +2,36 @@ package com.digitalpebble.classification.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
+
+import liblinear.Model;
 
 import com.digitalpebble.classification.Lexicon;
 
 public class ModelUtils {
   
-  public static void getAttributeScores(String model, String lexiconF)
+  public static void getAttributeScores(String modelPath, String lexiconF)
       throws IOException {
     // load the model + the lexicon
     // try to see if we can get a list of the best scores from the model
+    // works only for liblinear
     Lexicon lexicon = new Lexicon(lexiconF);
+    Model liblinearModel = Model.load(new File(modelPath));
+    double[] weights = liblinearModel.getFeatureWeights();
+    // dump all the weights
+    int numClasses = liblinearModel.getNrClass();
+    int numFeatures = liblinearModel.getNrFeature();
     
+    Map<Integer, String> invertedAttributeIndex = lexicon.getInvertedIndex();
+    
+    for (int i = 0; i< weights.length; i++){
+      // get current class num
+      int classNum = i / numFeatures;
+      int featNum = i % numFeatures;
+      String classLabel = lexicon.getLabel(classNum);
+      String attLabel = invertedAttributeIndex.get(featNum+1);
+      System.out.println(attLabel+"\t"+classLabel+"\t"+weights[i]);
+    }
   }
   
   public static void main(String[] args) {
